19e7a0063b1c09ef55a2685646cee8b67d26370a
Va1erii <valerapopovsakha@gmail.com>
2018-01-02 01:36:48 +0300

    HeroesActivity

---
+++ app/src/main/java/com/valeriipopov/jumanjiquiz/HeroesActivity.java
@@ -0,0 +1,85 @@
+package com.valeriipopov.jumanjiquiz;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class HeroesActivity extends AppCompatActivity {
+
+    public static final String STAGE_1 = "Stage 1";
+    public static final String STAGE_2 = "Stage 2";
+    public static final String STAGE_3 = "Stage 3";
+    public static final String STAGE_4 = "Stage 4";
+
+    private RelativeLayout mHeroBackground;
+    private TextView mStage;
+    private TextView mHeroName;
+    private TextView mHeroStrength;
+    private TextView mHeroWeakness;
+    private TextView mHeroHistory;
+    private Handler mHandler;
+    private Resources mResources;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heroes);
+        mHandler = new Handler(getMainLooper());
+        mResources = getResources();
+        mHeroBackground = findViewById(R.id.hero_background);
+
+        mHeroName = findViewById(R.id.hero_name);
+        mHeroStrength = findViewById(R.id.strengths);
+        mHeroWeakness = findViewById(R.id.weakness);
+        mHeroHistory = findViewById(R.id.hero_history);
+
+        mStage = findViewById(R.id.stage);
+        String stage = getIntent().getStringExtra(MainActivity.STAGE);
+        switch (stage){
+            case STAGE_1:
+                mHeroBackground.setBackground(mResources.getDrawable(R.drawable.bg_shelly));
+                mStage.setText(stage);
+                mHeroName.setText(mResources.getText(R.string.shelly));
+                mHeroStrength.setText(mResources.getText(R.string.shelly_strengths));
+                mHeroWeakness.setText(mResources.getText(R.string.shelly_weakness));
+                mHeroHistory.setText(mResources.getText(R.string.shelly_history));
+                break;
+            case STAGE_2:
+                mHeroBackground.setBackground(mResources.getDrawable(R.drawable.bg_ruby));
+                mStage.setText(stage);
+                mHeroName.setText(mResources.getText(R.string.ruby));
+                mHeroStrength.setText(mResources.getText(R.string.ruby_strengths));
+                mHeroWeakness.setText(mResources.getText(R.string.ruby_weakness));
+                mHeroHistory.setText(mResources.getText(R.string.ruby_history));
+                break;
+            case STAGE_3:
+                mHeroBackground.setBackground(mResources.getDrawable(R.drawable.bg_franklin));
+                mStage.setText(stage);
+                mHeroName.setText(mResources.getText(R.string.franklin));
+                mHeroStrength.setText(mResources.getText(R.string.franklin_strengths));
+                mHeroWeakness.setText(mResources.getText(R.string.franklin_weakness));
+                mHeroHistory.setText(mResources.getText(R.string.franklin_history));
+                break;
+            case STAGE_4:
+                mHeroBackground.setBackground(mResources.getDrawable(R.drawable.bg_bravestone));
+                mStage.setText(stage);
+                mHeroName.setText(mResources.getText(R.string.bravestone));
+                mHeroStrength.setText(mResources.getText(R.string.bravestone_strengths));
+                mHeroWeakness.setText(mResources.getText(R.string.bravestone_weakness));
+                mHeroHistory.setText(mResources.getText(R.string.bravestone_history));
+                break;
+        }
+        mStage.setText(stage);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(HeroesActivity.this, QuestionActivity.class));
+            }
+        }, 10000);
+    }
+}
--- app/src/main/java/com/valeriipopov/jumanjiquiz/MainActivity.java
+++ app/src/main/java/com/valeriipopov/jumanjiquiz/MainActivity.java
@@ -1,13 +1,68 @@
 package com.valeriipopov.jumanjiquiz;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String STAGE = "stage";
+
+    private Button mStage1;
+    private Button mStage2;
+    private Button mStage3;
+    private Button mStage4;
+    private Resources mResources;
+    private ArrayList <Button> mButtons;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mResources = getResources();
+        mHandler = new Handler(getMainLooper());
+
+        mStage1 = findViewById(R.id.btn_stage_1);
+        mStage2 = findViewById(R.id.btn_stage_2);
+        mStage3 = findViewById(R.id.btn_stage_3);
+        mStage4 = findViewById(R.id.btn_stage_4);
+        setBtnOnClick();
+    }
+
+    private void setBtnOnClick(){
+        mButtons = new ArrayList<>();
+        mButtons.add(mStage1);
+        mButtons.add(mStage2);
+        mButtons.add(mStage3);
+        mButtons.add(mStage4);
+        for (final Button btn: mButtons){
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    btn.setTextColor(mResources.getColor(R.color.colorSelectTextJumanji));
+                    mHandler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            btn.setTextColor(mResources.getColor(R.color.colorTextSubTitle));
+                        }
+                    }, 100);
+                    mHandler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent = new Intent(MainActivity.this, HeroesActivity.class);
+                            intent.putExtra(STAGE, btn.getText().toString());
+                            startActivity(intent);
+                        }
+                    }, 200);
+                }
+            });
+        }
     }
 }
